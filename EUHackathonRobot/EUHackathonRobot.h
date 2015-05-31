61a0453fe052fc15a5765a394c05d9b134fa7213
Shabir Mohamed <shabir_tck09@hotmail.com>
2015-05-31 20:44:44 +0530

    EU Hackathon SenseBot Arduino Agent (Need to add motor control code)

---
+++ EUHackathonRobot/EUHackathonRobot.h
@@ -0,0 +1,52 @@
+#ifndef EUHackathonRobot_H
+#define EUHackathonRobot_H
+
+#if (ARDUINO >= 100)
+ #include "Arduino.h"
+#else
+ #include "WProgram.h"
+#endif
+
+// These are the interrupt and control pins
+#define ADAFRUIT_CC3000_IRQ   3  // MUST be an interrupt pin!
+// These can be any two pins
+#define ADAFRUIT_CC3000_VBAT  5
+#define ADAFRUIT_CC3000_CS    10
+
+#define WLAN_SSID       "WSO2-Restricted"           // cannot be longer than 32 characters!
+#define WLAN_PASS       "LKvene8xIOT"
+
+#define WLAN_SECURITY   WLAN_SEC_WPA2
+                           // Security can be WLAN_SEC_UNSEC, WLAN_SEC_WEP, WLAN_SEC_WPA or WLAN_SEC_WPA2
+#define IDLE_TIMEOUT_MS  3000      
+
+#define DEVICE_OWNER "Test" //"${DEVICE_OWNER}"          //"SHABIRMEAN"
+#define DEVICE_ID "Test" //"${DEVICE_ID}"              //"vbhenqyt85yq"
+#define DEVICE_TOKEN "${DEVICE_TOKEN}"
+
+
+#define SERVICE_PORT 9763 
+#define SERVICE_EPOINT "/iotdevices/SenseBotController/" 
+                                        // pushalarmdata - application/json - {"owner":"","deviceId":"","replyMessage":"","time":"","key":"","value":""}
+                                        // readcontrols/{owner}/{deviceId}
+                                        // reply - application/json - {"owner":"","deviceId":"","replyMessage":""}
+
+#define SONAR_TRIG  2
+#define SONAR_ECHO  4
+#define PIR_PIN  12
+#define LDR_PIN  A1
+#define TEMP_PIN 13
+
+#define BUZZER 6
+#define BUZZER_SOUND 100
+#define MAX_DISTANCE 30
+
+#define TURN_DELAY 100
+
+#define POLL_INTERVAL 1000
+#define DEBUG true
+#define CON_DEBUG true
+
+#endif
+
+
---
+++ EUHackathonRobot/EUHackathonRobot.ino
@@ -0,0 +1,69 @@
+#include "EUHackathonRobot.h"
+
+#include <Adafruit_CC3000.h>
+#include <SPI.h>
+#include "dht.h"
+#include <pt.h>
+
+Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ, ADAFRUIT_CC3000_VBAT,
+                                         SPI_CLOCK_DIVIDER); // you can change this clock speed
+
+Adafruit_CC3000_Client pushClient;
+
+static struct pt pushThread;
+
+    /**********************************************************************************************  
+        0. Check with a sample Wifi code of the Adafruit_CC3000 library to ensure that the sheild is working
+        1. Set the ip of the server(byte array below) where the Web-Rest API for the FireAlarm is running
+        2. Check whether the "SERVICE_EPOINT" is correct in the 'FireAlarmWifiAgent.h' file
+        3. Check whether the "SERVICE_PORT" is the same (9763) for the server running. Change it if needed
+        4. Check whether the pins have been attached accordingly in the Arduino
+        5. Check whether all reqquired pins are added to the 'digitalPins' array  
+    ***********************************************************************************************/
+
+uint32_t sserver;
+byte server[4] = { 10, 100, 7, 38 };
+
+String host, jsonPayLoad;
+
+void setup() {
+  if(true) Serial.begin(115200); 
+  pinMode(PIR_PIN, INPUT);
+
+//  PT_INIT(&pushThread);
+  
+  connectHttp();
+  setupResource();
+}
+
+void loop() {
+  if (pushClient.connected()) {   
+    pushData();                    // batches all the required pin values together and pushes once
+//    protothread1(&pushThread, 1000);      // Pushes data and waits for control signals to be received
+    delay(POLL_INTERVAL);
+    
+  } else {
+    if(DEBUG) {
+      Serial.println("client not found...");
+      Serial.println("disconnecting.");
+    }
+    pushClient.close();
+    cc3000.disconnect();  
+   
+    connectHttp();
+  }  
+}
+
+
+static int protothread1(struct pt *pt, int interval) {
+  static unsigned long timestamp = 0;
+  PT_BEGIN(pt);
+  while(1) { // never stop 
+    /* each time the function it is checked whether any control signals are sent
+    *  if so exit this proto thread
+    */
+//    PT_WAIT_UNTIL(pt, readControls() );
+//    pushData();
+  }
+  PT_END(pt);
+}
